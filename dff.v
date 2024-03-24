b69d8f04923db9fd94aa7aac4e285f49b0be1a9c
Tarakaprabu <140338143+Tarakaprabu@users.noreply.github.com>
2024-03-24 22:15:18 +0530

    Add files via upload

---
+++ dff.v
@@ -0,0 +1,13 @@
+module dff(d,clk,rst,q,qbar);
+  input d,clk,rst;
+  output reg q, qbar;
+  always@(posedge clk)
+    begin
+      if(rst)
+        q<=1'b0;
+      else
+        q<=d;
+    end
+  always@(q)
+    qbar = ~q;
+endmodule
